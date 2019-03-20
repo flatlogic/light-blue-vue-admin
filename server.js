33eb08bc485c664fa83a090d1563ab1290d15bd0
okendoken <philip@flatlogic.com>
2019-03-20 17:44:25 +0300

    deploy light blue vue admin 1.0

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var serveStatic = require('serve-static');
-
-var app = express();
-app.use(serveStatic(__dirname + "/dist"));
-
-var port = process.env.PORT || 5000;
-app.listen(port);
